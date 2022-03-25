829157e9ce4247e51f00340a16dab25b987f656d
Ramiro Nahuel Perez <ramironahuel101@gmail.com>
2022-03-25 08:55:06 -0300

    se realizo la pagina 404

---
+++ src/components/views/Pag404/Pagina404.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import error from "../../Assets/Img404/error.jpg";
+import "./Pagina404.css";
+
+
+const Pagina404 = () => {
+  return (
+    <div>
+      <div className="E404">
+        <img className=" Error404 my-3" src={error} />
+        <div>
+          <Link to="/" className="btn-ingresar text-decoration-none text-center my-2">Volver </Link>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Pagina404;
