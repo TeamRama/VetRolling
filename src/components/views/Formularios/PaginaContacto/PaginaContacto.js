179a3cd97ebcb3e63524226757681f5dc36327c5
Ramiro Nahuel Perez <ramironahuel101@gmail.com>
2022-04-02 16:16:41 -0300

    .

--- src/components/views/Formularios/PaginaContacto/PaginaContacto.js
+++ src/components/views/Formularios/PaginaContacto/PaginaContacto.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Container, Form, Row , Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import emailjs from '@emailjs/browser';
+import { Container, Form, Row, Col } from "react-bootstrap";
+
 import "./PaginaContacto.css";
 import "../../../../Styles/GeneralStyles.css";
 
 
+
+
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('service_id', 'template_id', 'user_id')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+};
 
 
 const PaginaContacto = () => {
@@ -15,15 +29,17 @@ const PaginaContacto = () => {
                 <hr />
                 {/* Form Contacto */}
                 <div className='Form-Contacto'>
-                    <Form className="p-3 m-3">
+                    <Form className="p-3 m-3"  onSubmit={sendEmail}>
                         <Row ClassName='RowContacto' >
                             <Col md={6}>
-                                <Form.Group className="my-3  groupcontacto" controlId="formBasicEmail">
+                                <Form.Group className="my-3  groupcontacto" controlId="formBasicEmail"
+                                >
                                     <Form.Label>Nombre y Apellido</Form.Label>
                                     <Form.Control
                                         className="imputcontacto"
                                         type="text"
-                                        placeholder=" Nombre Completo" />
+                                        placeholder=" Nombre Completo"
+                                        name="NombreCompleto" />
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
@@ -32,7 +48,8 @@ const PaginaContacto = () => {
                                     <Form.Control
                                         className="imputcontacto"
                                         type="Email"
-                                        placeholder="Correo Electronico " />
+                                        placeholder="Correo Electronico"
+                                        name="Email" />
 
                                 </Form.Group>
                             </Col>
@@ -43,7 +60,8 @@ const PaginaContacto = () => {
                                     <Form.Control
                                         className="imputcontacto"
                                         type="tel"
-                                        placeholder="Celular de Contacto" />
+                                        placeholder="Celular de Contacto"
+                                        name="Celular" />
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
@@ -52,7 +70,8 @@ const PaginaContacto = () => {
                                     <Form.Control
                                         className="imputcontacto"
                                         type="text"
-                                        placeholder="Provincia - Localidad" />
+                                        placeholder="Provincia - Localidad"
+                                        name="Provincia" />
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
@@ -61,13 +80,16 @@ const PaginaContacto = () => {
                                     <Form.Control
                                         className="imputcontacto"
                                         type="text"
-                                        placeholder=" Calle y Numeracion" />
+                                        placeholder=" Calle y Numeracion"
+                                        name="Domicilio" />
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
                                 <Form.Group className="my-3 groupcontacto" controlId="formBasicCheckbox">
                                     <Form.Label>Tipo de Plan</Form.Label>
-                                    <Form.Select className="imputcontacto" >
+                                    <Form.Select
+                                        className="imputcontacto"
+                                        name="Plan">
                                         <option value="">Me interesa el Plan </option>
                                         <option value="cachorro">Primeros Pasos</option>
                                         <option value="maduro">Madurando</option>
@@ -77,8 +99,7 @@ const PaginaContacto = () => {
                                 </Form.Group>
                             </Col>
                             <div className="text-end">
-                                <Link to="/PaginaContacto" className="btn-reservar text-decoration-none text-center"> Me Interesa </Link>
-
+                            <input type="submit" value="Send" />
                             </div>
                         </Row>
                     </Form>
